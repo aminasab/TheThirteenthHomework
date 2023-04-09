06a771458a08701f70026e95ce0c7c4976068117
Amina S <aminchik2809@mail.ru>
2023-04-10 01:08:26 +0300

    add files

---
+++ IEnumerableExtension.cs
@@ -0,0 +1,40 @@
+﻿namespace TheThirteenthProgram
+{
+    public static class IEnumerableExtension
+    {
+        /// <summary>
+        /// Возвращает заданное количество процентов от выборки с округлением количества элементов в большую сторону.
+        /// </summary>
+        public static IEnumerable<T> Top<T>(this IEnumerable<T> source, int x)
+        {
+            if (source is null || !source.Any())
+            {
+                throw new ArgumentException("Вывод результата невозможен из-за отсутствия данных.");
+            }
+            if (x > 100 || x < 1)
+            {
+                throw new ArgumentException("Введеное значение должно быть в диапазоне от 1 до 100.");
+            }
+            double percent = source.Count() * x / 100.00;
+            int value = (int)Math.Ceiling(percent);
+            var result = source.TakeLast(value).OrderByDescending(x => x);
+            return result;
+        }
+
+        public static IEnumerable<T> Top<T, TKey>(this IEnumerable<T> source, int x, Func<T, TKey> keySelector)
+        {
+            if (source is null || !source.Any())
+            {
+                throw new ArgumentException("Вывод результата невозможен из-за отсутствия данных.");
+            }
+            if (x > 100 || x < 1)
+            {
+                throw new ArgumentException("Введеное значение должно быть в диапазоне от 1 до 100.");
+            }
+            double parcent = source.Count() * x / 100.00;
+            int value = (int)Math.Ceiling(parcent);
+            var result = source.OrderByDescending(keySelector).Take(value);
+            return result;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿namespace TheThirteenthProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var listOfNumbers = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            foreach (var number in (listOfNumbers.Top(30)))
+            {
+                Console.WriteLine(number);
+            }
+            var listOfPeople = new List<Person>
+            {
+            new Person ("Fill",66),
+            new Person("Jack",90),
+            new Person("Elen",43),
+            new Person("Kate",12)
+            };
+            foreach (var human in (listOfPeople.Top(30, person => person.Age)))
+            {
+                Console.WriteLine(human.Name + " " + human.Age);
+            }
+        }
+    }
+}
